ee51d15496661af6ab8b1f139da983a13b859482
CodeDecodes <kaushal.digraskar@weagile.net>
2022-09-01 18:10:02 +0530

    Added new changes in footer and header tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,31 +10,41 @@ export class AppComponent {
   dynamicSlides = [
     {
       id: 1,
-      src: 'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg',
+      images: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
+        () => `https://picsum.photos/900/500?random&t=${Math.random() - 1}`
+      ),
       alt: 'Image_1',
       title: 'Image_1',
     },
     {
       id: 2,
-      src: 'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg',
+      images: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(
+        () => `https://picsum.photos/900/500?random&t=${Math.random() + 2}`
+      ),
       alt: 'Image_2',
       title: 'Image_3',
     },
     {
       id: 3,
-      src: 'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg',
+      images: [1, 2, 3, 4, 5, 6, 7, 8, 9].map(
+        () => `https://picsum.photos/900/500?random&t=${Math.random() + 1}`
+      ),
       alt: 'Image_3',
       title: 'Image_3',
     },
     {
       id: 4,
-      src: 'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg',
+      images: [1, 2, 3, 4, 5, 6, 7, 8, 9].map(
+        () => `https://picsum.photos/900/500?random&t=${Math.random() + 4}`
+      ),
       alt: 'Image_4',
       title: 'Image_4',
     },
     {
       id: 5,
-      src: 'https://i.picsum.photos/id/705/400/250.jpg',
+      images: [1, 2, 3, 4, 5, 6, 7, 8, 9].map(
+        () => `https://picsum.photos/900/500?random&t=${Math.random()}`
+      ),
       alt: 'Image_5',
       title: 'Image_5',
     },
@@ -65,13 +75,10 @@ export class AppComponent {
       0: {
         items: 1,
       },
-      600: {
-        items: 1,
-      },
-      1000: {
-        items: 1,
-      },
     },
   };
   items = [{ title: 'Profile' }, { title: 'Logout' }];
+  ngOnIt() {
+    console.log('https://picsum.photos/900/500?random&t=${Math.random() + 4}');
+  }
 }
